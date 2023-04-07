d01cff8bd1242404f5ea2d0df261efc6f8f4a32c
Gabriel <gabrie.kuka@gmail.com>
2023-04-07 21:01:33 +0300

    style: Added add-template page and the form in a separate page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard/dashboard";
+import Template from "./components/templates";
 import NotFound from "./components/notfound";
 import Accounts from "./components/accounts";
 import Profile from "./components/profile";
@@ -10,23 +11,24 @@ import GlobalProvider from "./context/GlobalContext";
 import Register from "./components/authentication/register";
 
 function App() {
-  return (
-    <>
-      <GlobalProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/accounts" element={<Accounts />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="" element={<Register />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </GlobalProvider>
-    </>
-  );
+    return (
+        <>
+            <GlobalProvider>
+                <Navbar />
+                <Routes>
+                    <Route path='/dashboard' element={<Dashboard />} />
+                    <Route path='/template' element={<Template />} />
+                    <Route path='/' element={<Dashboard />} />
+                    <Route path='/accounts' element={<Accounts />} />
+                    <Route path='/profile' element={<Profile />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='' element={<Register />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </GlobalProvider>
+        </>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/templates.jsx
@@ -0,0 +1,181 @@
+import React, { useState, useEffect } from "react";
+import { Formik, Form, Field } from "formik";
+import "./templates.scss";
+import transactionService from "../services/transactionService";
+
+const Template = () => {
+    const types = ["Income", "Expense", "Transfer"];
+    const [accounts, setAccounts] = useState([]);
+    const [transfers, setTransfers] = useState([]);
+
+    const [incomeCategories, setIncomeCategories] = useState([]);
+    const [expenseCategories, setExpenseCategories] = useState([]);
+
+    useEffect(() => {
+        getAccounts();
+        getTransfers();
+        getIncomeCategories();
+        getExpenseCategories();
+    }, []);
+
+    async function getTransfers() {
+        const response = await transactionService.getAllUserTransfers();
+        setTransfers(response);
+    }
+
+    async function getAccounts() {
+        const response = await transactionService.getAllUserAccounts();
+        setAccounts(response);
+    }
+
+    async function getIncomeCategories() {
+        const response = await transactionService.getAllIncomeCategories();
+        setIncomeCategories(response);
+    }
+
+    async function getExpenseCategories() {
+        const response = await transactionService.getAllExpenseCategories();
+        setExpenseCategories(response);
+    }
+
+    return (
+        <div className={"add-template-wrapper"}>
+            <Formik
+                initialValues={{
+                    amount: "",
+                    description: "",
+                    date: new Date().toISOString().slice(0, 10),
+                    account: "",
+                    from_account: "",
+                    to_account: "",
+                    income_category: "",
+                    expense_category: "",
+                    type: "",
+                }}
+                onSubmit={async (values, { setSubmitting, resetForm }) => {
+                    console.log(values);
+                    setSubmitting(false);
+                    resetForm();
+                }}
+            >
+                {({ values }) => (
+                    <Form className={"form"}>
+                        <label>Add Template</label>
+                        <Field
+                            id='date'
+                            name='date'
+                            placeholder='Enter date'
+                        />
+                        <Field as='select' name='type' id='type'>
+                            <option value='' disabled hidden>
+                                Select type
+                            </option>
+                            {types?.map((t) => (
+                                <option
+                                    key={types.indexOf(t)}
+                                    value={types.indexOf(t)}
+                                >
+                                    {t}
+                                </option>
+                            ))}
+                        </Field>
+                        {parseInt(values.type) === 0 && (
+                            <>
+                                <Field as='select' name='account'>
+                                    <option value='' disabled hidden>
+                                        Select account
+                                    </option>
+                                    {accounts?.map((a) => (
+                                        <option key={a.id} value={a.id}>
+                                            {a.name}{" "}
+                                            {parseFloat(a.amount).toFixed(2)} €
+                                        </option>
+                                    ))}
+                                </Field>
+                                <Field as='select' name='income_category'>
+                                    <option value='' disabled hidden>
+                                        Income category
+                                    </option>
+                                    {incomeCategories?.map((c) => (
+                                        <option key={c.id} value={c.id}>
+                                            {c.category_type}
+                                        </option>
+                                    ))}
+                                </Field>
+                            </>
+                        )}
+
+                        {parseInt(values.type) === 1 && (
+                            <>
+                                <Field as='select' name='account'>
+                                    <option value='' disabled hidden>
+                                        Select account
+                                    </option>
+                                    {accounts?.map((a) => (
+                                        <option key={a.id} value={a.id}>
+                                            {a.name}{" "}
+                                            {parseFloat(a.amount).toFixed(2)} €
+                                        </option>
+                                    ))}
+                                </Field>
+                                <Field as='select' name='expense_category'>
+                                    <option value='' disabled hidden>
+                                        Expense category
+                                    </option>
+                                    {expenseCategories?.map((c) => (
+                                        <option key={c.id} value={c.id}>
+                                            {c.category_type}
+                                        </option>
+                                    ))}
+                                </Field>
+                            </>
+                        )}
+                        {parseInt(values.type) === 2 && (
+                            <>
+                                <Field as='select' name='from_account'>
+                                    <option value='' disabled hidden>
+                                        From account
+                                    </option>
+                                    {accounts &&
+                                        accounts.map((a) => (
+                                            <option key={a.id} value={a.id}>
+                                                {a.name}{" "}
+                                                {parseFloat(a.amount).toFixed(
+                                                    2
+                                                )}{" "}
+                                                €
+                                            </option>
+                                        ))}
+                                </Field>
+                                <Field as='select' name='to_account'>
+                                    <option value='' disabled hidden>
+                                        To account
+                                    </option>
+                                    {accounts &&
+                                        accounts.map((a) => (
+                                            <option key={a.id} value={a.id}>
+                                                {a.name}{" "}
+                                                {parseFloat(a.amount).toFixed(
+                                                    2
+                                                )}{" "}
+                                                €
+                                            </option>
+                                        ))}
+                                </Field>
+                            </>
+                        )}
+                        <Field name='amount' placeholder='Enter amount' />
+                        <Field
+                            type='text'
+                            name='description'
+                            placeholder='Enter description'
+                        />
+                        <button type='submit'>Submit</button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
+};
+
+export default Template;
