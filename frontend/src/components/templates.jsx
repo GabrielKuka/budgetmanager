d6f4bc41a98cf3b39e8663b821d58885dee2d3d0
Gabriel <gabrie.kuka@gmail.com>
2023-04-16 14:27:55 +0300

    style: Design Improvements.

--- frontend/src/components/templates.jsx
+++ frontend/src/components/templates.jsx
@@ -56,7 +56,12 @@ const Template = () => {
                 templateGroups={templateGroups}
             />
             {templateGroups?.length > 0 && (
-                <TemplateGroups templateGroups={templateGroups} />
+                <TemplateGroups
+                    templateGroups={templateGroups}
+                    accounts={accounts}
+                    incomeCategories={incomeCategories}
+                    expenseCategories={expenseCategories}
+                />
             )}
         </div>
     );
@@ -87,19 +92,119 @@ const Sidebar = (props) => {
 };
 
 const TemplateGroups = (props) => {
+    const [currentTemplateGroup, setCurrentTemplateGroup] = useState(false);
+
+    function currentTemplateGroupStyle() {
+        // Get the item we're hovering over
+        const currentTemplateGroupElement = document.getElementById(
+            `template-group-item-${currentTemplateGroup.id}`
+        );
+
+        // Get item's position
+        const rect = currentTemplateGroupElement.getBoundingClientRect();
+
+        return {
+            border: "1px solid cadetblue",
+            boxShadow: "1px 1px 3px #dadada",
+            borderRadius: "5px",
+            position: "absolute",
+            left: "105%",
+            top: `${rect.top - 80}px`,
+            width: "300px",
+            padding: "10px",
+        };
+    }
+
     return (
         <div className={"template-wrapper__template-groups"}>
             <div className={"header"}>
                 <label>Name</label>
             </div>
             <div className={"template-groups"}>
-                {console.log(props.templateGroups)}
                 {props.templateGroups?.map((t) => (
-                    <div key={t.id} className={"template-group-item"}>
+                    <div
+                        key={t.id}
+                        onMouseEnter={() => setCurrentTemplateGroup(t)}
+                        onMouseLeave={() => setCurrentTemplateGroup(false)}
+                        className={"template-group-item"}
+                        id={`template-group-item-${t.id}`}
+                    >
                         <label>{t.name}</label>
                     </div>
                 ))}
             </div>
+            {currentTemplateGroup && (
+                <div
+                    style={currentTemplateGroupStyle()}
+                    className={"current-template-group"}
+                    id={"current-template-group"}
+                >
+                    {currentTemplateGroup.template_group.map((i) => (
+                        <TemplateItem
+                            key={i.id}
+                            i={i}
+                            accounts={props.accounts}
+                            incomeCategories={props.incomeCategories}
+                            expenseCategories={props.expenseCategories}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+const TemplateItem = (props) => {
+    const type = () => {
+        if (props.i.type == 0) return "income";
+        if (props.i.type == 1) return "expense";
+        if (props.i.type == 2) return "transfer";
+    };
+
+    const allAccounts = props.accounts;
+
+    const account = props.i.account;
+    const from_account = props.i.from_account;
+    const to_account = props.i.to_account;
+
+    const amount = props.i.amount;
+    const category = props.i.category;
+
+    function getAccount(id) {
+        const res = allAccounts.filter((a) => a.id == id);
+        return res.length == 1 ? res[0].name : "";
+    }
+    function getCategory(id) {
+        if (props.i.type == 0) {
+            const res = props.incomeCategories.filter((c) => c.id == id);
+            return res.length == 1 ? res[0].category_type : "";
+        }
+        if (props.i.type == 1) {
+            const res = props.expenseCategories.filter((c) => c.id == id);
+            return res.length == 1 ? res[0].category_type : "";
+        }
+    }
+    return (
+        <div className={"template-item"}>
+            {type() == "income" && (
+                <label>
+                    Earn <b>{amount}€</b> to <i>{getAccount(account)}</i> as{" "}
+                    {getCategory(category)}
+                </label>
+            )}
+            {type() == "expense" && (
+                <label>
+                    Spend <b>{amount}€</b> from <i>{getAccount(account)}</i> on{" "}
+                    {getCategory(category)}
+                </label>
+            )}
+            {type() == "transfer" && (
+                <label>
+                    Transfer <b>{amount}€</b> from{" "}
+                    <i>{getAccount(from_account)}</i> to{" "}
+                    <i>{getAccount(to_account)}</i>
+                </label>
+            )}
         </div>
     );
 };
