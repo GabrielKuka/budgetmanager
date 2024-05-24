cd2b65b882479fab9c58b496deda9e7c0a8f0bfe
Gabriel <gabrie.kuka@gmail.com>
2024-05-25 00:32:00 +0300

    fix: default currency selected in the conversioion tool

--- frontend/src/components/core/conversiontool.jsx
+++ frontend/src/components/core/conversiontool.jsx
@@ -4,7 +4,7 @@ import currencyService from "../../services/currencyService";
 import "./conversiontool.scss";
 
 const ConversionTool = ({ closePopup }) => {
-  const currencies = ["BGN", global.globalCurrency, "USD", "GBP"];
+  const currencies = ["BGN", "EUR", "USD", "GBP"];
   const [result, setResult] = useState(null);
 
   return (
@@ -21,7 +21,7 @@ const ConversionTool = ({ closePopup }) => {
           initialValues={{
             amount: 0,
             from_currency: "BGN",
-            to_currency: global.globalCurrency,
+            to_currency: "EUR",
           }}
           onSubmit={async (values) => {
             const from = values.from_currency;
